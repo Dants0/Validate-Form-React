71634cd6e6693dd121bc690c246069c6736bd270
Guilherme Dantas <guilhermedantasgoes@gmail.com>
2022-03-09 11:54:38 -0300

    comment files

--- src/App.jsx
+++ src/App.jsx
@@ -14,4 +14,23 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
+
+
+// import React, {useState} from 'react';
+// import './App.css';
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const n=5;
+
+//   return (
+//     <div className="App">
+//       <p>Voce clicou {count} vezes</p>
+//       <p>Voce clicou {count * n}</p>
+//       <button onClick={()=> setCount(count+1)}>Click</button>
+//     </div>
+//   );
+// }
+
+// export default App;
