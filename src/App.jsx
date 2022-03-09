35bc54684d02b9451f9523e1cf01ecd01b841385
Guilherme Dantas <guilhermedantasgoes@gmail.com>
2022-03-09 11:46:43 -0300

    add script

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const n=5;
+
+  return (
+    <div className="App">
+      <p>Voce clicou {count} vezes</p>
+      <p>Voce clicou {count * n}</p>
+      <button onClick={()=> setCount(count+1)}>Click</button>
+    </div>
+  );
+}
+
+export default App;
