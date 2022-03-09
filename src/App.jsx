aa33e30c25ce1222f53e1d5330cb3d74ec508c6e
Guilherme Dantas <guilhermedantasgoes@gmail.com>
2022-03-09 11:55:37 -0300

    init validate-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const n=5;
-
   return (
     <div className="App">
-      <p>Voce clicou {count} vezes</p>
-      <p>Voce clicou {count * n}</p>
-      <button onClick={()=> setCount(count+1)}>Click</button>
+      <form action="">
+        
+        
+        
+      </form>
     </div>
   );
 }
