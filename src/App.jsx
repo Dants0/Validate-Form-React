1fb47d621e6b5a17c652072647233e4025e74fec
Guilherme Dantas <guilhermedantasgoes@gmail.com>
2022-03-10 12:22:34 -0300

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -90,21 +90,3 @@ function App() {
 export default App;
 
 
-//React Hooks count example
-// import React, {useState} from 'react';
-// import './App.css';
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const n=5;
-
-//   return (
-//     <div className="App">
-//       <p>Voce clicou {count} vezes</p>
-//       <p>Voce clicou {count * n}</p>
-//       <button onClick={()=> setCount(count+1)}>Click</button>
-//     </div>
-//   );
-// }
-
-// export default App;
